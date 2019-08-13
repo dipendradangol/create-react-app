6365de1de28a2ae395258909c3e3135c9b326fce
Dipendra Dangol <dipendra.dangol11@gmail.com>
2019-08-13 14:14:21 +0300

    initial commit, add hello world app

---
+++ hello-world/src/index.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+/* 
+const customStyle = {
+    'color' : 'yellow',
+    'background-color' : 'grey'
+    
+};
+ */
+/* 
+const helloMessage = <div>
+    <img src='./images/death-valley.jpg'></img>
+    <h4>Dipendra dangol</h4>
+    <div style={ customStyle}>64 likes</div>
+    </div>
+
+ */
+/* const imgSrc = <img src = './images/lapland'></img>;
+
+const helloMessage = React.createElement(
+    'div',
+    null,
+    [
+        React.createElement(
+            'img',{
+                key: 0,
+                src : imgSrc
+            },
+            null
+            
+            
+        ),
+        React.createElement(
+            'h4',
+            {key : 1},
+            'Dipendra Dangol'
+
+        ),
+        React.createElement(
+            'div',
+            {key : 2,
+            style : customStyle
+            },
+            '64 likes'
+        )
+    ]
+); */
+
+/**
+ * something
+ */
+class helloMessage extends React.Component{
+    render(){
+        return (
+            <div class ='btn'>
+                <h1>Hello from react component></h1>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <helloMessage/>, //helloMessage, <h1>Hello world</h1>
+    document.getElementById('root')
+);
+
+
+
+// JSX = javascript as XML to create react elements
+
+
