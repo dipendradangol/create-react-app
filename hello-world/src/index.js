fe4d69744c8f4ae3de04c0f7fb8ad1f4b5f99fe3
Dipendra Dangol <dipendra.dangol11@gmail.com>
2019-08-14 13:50:46 +0300

    composing componets

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { component } from 'react';
+import { render } from 'react-dom'; 
+
 /* 
 const customStyle = {
     'color' : 'yellow',
@@ -49,23 +50,128 @@ const helloMessage = React.createElement(
 /**
  * something
  */
-class HelloMessage extends React.Component{
+
+ //
+
+/* class HelloMessage extends React.Component{
     render(){
+        console.log(this.props)
+
         return (
-            <div class ='btn'>
-                <h1>Hello from react component></h1>
+            <div>
+                <h1>{this.props.msg}</h1>
+                <h1>{this.props.msg2}</h1>
             </div>
         )
     }
 }
 
 ReactDOM.render(
-    <HelloMessage/>, //helloMessage, <h1>Hello world</h1>
+    <HelloMessage msg= "Good Morning" msg2= " Good Evening" />, 
     document.getElementById('root')
 );
+ */
+
+
+/* 
+const greetingMessages = {
+    moring : ' Good Morning ',
+    afternoon : ' Good Afternoon ',
+    evening : ' Good Evening ',
+}
+
+const  today = new Date();
 
 
 
-// JSX = javascript as XML to create react elements
+class HelloMessage extends React.Component{
+    
+    fetchGreetingMessage = time => {
+        if (time <= 12){
+            return this.props.morningMessage;
+        } else if (time<= 18) {
+            return this.props.afternoonMessage;
+        } else {
+            return this.props.eveningMessage;
+        }
+    }
+    
+    render(){
+        return (
+            <div>
+            <h1>{this.fetchGreetingMessage(this.props.timeNow)}</h1>
+            <h1>the time now is {this.props.today}</h1>
+            
+            </div>
+            )
+        }
+    }
+
+ReactDOM.render(
+    <HelloMessage 
+        morningMessage = {greetingMessages.morning}
+        afternoonMessage = {greetingMessages.afternoon}
+        eveningMessage = {greetingMessages.evening}
+        //timeNow ={hour}
+        //today={now}
+     />, 
+    document.getElementById('root')
+);
+    */
+   
 
 
+/* const Movie = ( props ) => {
+    return (<div>
+        <h2>{props.title}</h2>
+        <p><{props.imdb}/p>
+        <p><{props.starting}/p>
+    </div>)
+} */
+
+// excercises
+
+const movieList =[ 
+    {
+        title: "Twilight",
+        imdb : "8",
+        starting : " Kristen, robert ",
+    },
+    {
+        title: "Avatar",
+        imdb : "7",
+        starting : " Jake, Grace",
+    },
+    {
+        title: "harry potter",
+        imdb : "9",
+        starting : " emma watson, rupert, tom ",
+    },
+];
+
+function Movie(props) {
+    return <div>
+        <h2>{props.title}</h2>
+        <p>{props.imdb}</p>
+        <p>{props.staring}</p>
+    </div>
+}
+
+function Cinema(props){ //composing component
+    console.log(props);
+    return <div>
+        {props.movies.map( (movie, index) => {
+            return <Movie 
+                key = {index}
+                title = {movie.title} 
+                imdb = {movie.imdb}
+                staring = {movie.staring}
+            />
+        })}
+    </div>
+}
+
+render(
+    <Cinema movies = {movieList}/>,
+    document.getElementById('root')
+);
