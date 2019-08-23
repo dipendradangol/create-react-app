586c8bb10dd2244132a02100147cfbbdd48251a1
Dipendra Dangol <dipendra.dangol11@gmail.com>
2019-08-23 12:22:02 +0300

    combine the 2 reducers and check the output of getState

---
+++ redux-app1/src/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { combineReducers, createStore } from 'redux';
+
+
+function productReducer(state = ['default1', 'default2'], action){
+        return state;  
+}
+function userReducer(state = 'defaultState', action){
+        return state;  
+}
+
+
+const allReducers = combineReducers ({
+    product: productReducer,
+    user: userReducer
+});
+
+const action = {
+    type: 'updateState',
+    payload: {
+        newState: 'New State'
+    }
+};
+const store = createStore(allReducers);
+
+console.log(store.getState()); // state of our store before dispatch the action
+
+store.dispatch(action);
+
+console.log(store.getState()); // state of our store after dispatch the action
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
